9aed59054c98a5135c6ca3114cc47bf18246d216
Shayan <shayan@Shayans-MacBook-Air.local>
2018-09-23 00:43:57 -0600

    basic ga

---
+++ basic.py
@@ -0,0 +1,117 @@
+import subprocess
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+DNA_SIZE = 13            # DNA length
+POP_SIZE = 100           # population size
+CROSS_RATE = 0.8         # mating probability (DNA crossover)
+MUTATION_RATE_META = 0.003    # mutation probability
+N_GENERATIONS = 200
+
+
+POPULATION = np.arange(10,160,10)
+CROSSOVER_RATE = np.arange(0.25,1.00,0.05)
+MUTATION_RATE = [0]
+for i in range(0,-7,-1):
+    MUTATION_RATE.append(2**i)
+#GENERATION_GAP = np.arange(0.3,1,0.1)
+#SCALING_WINDOW = np.arange(8)
+SELECTION_STRATEGY = ['RANK', 'ROULETTEWHEEL', 'TOURNAMENT', 'BINARY_TOURNAMENT']
+
+
+def branch_coverage(population_size, crossover_rate, mutation_rate, selection_strategy):
+
+    process = subprocess.Popen(
+        ['java', '-jar', '/home/ubuntu/evosuite-1.0.6.jar',
+         '-target', 'tullibee.jar',
+         '-Dcrossover_rate={}'.format(crossover_rate), '-Dpopulation={}'.format(population_size),
+         '-Dmutation_rate={}'.format(mutation_rate), '-Dselection_function'.format(selection_strategy),
+         '-Dsearch_budget=20', '-Dshow_progress=False'],
+        cwd='/home/ubuntu/SF100/1_tullibee')
+    process.wait()
+    # process = subprocess.Popen(
+    #     ['java', '-jar', 'C:/Users/Shayan Z/Downloads/SF100-EvoSuite-20120316/1_tullibee/evosuite-1.0.6.jar',
+    #      '-target', 'tullibee.jar',
+    #      '-criterion', 'branch', '-Dcrossover_rate={}'.format(crossover), '-Dpopulation={}'.format(population),
+    #      '-Dsearch_budget=20', '-Doutput_variables=BranchCoverage', '-Dshow_progress=False'],
+    #     cwd='C:/Users/Shayan Z/Downloads/SF100-EvoSuite-20120316/1_tullibee')
+    file = pd.read_csv(
+        '/home/ubuntu/SF100/1_tullibee/evosuite-report/statistics.csv')
+    r, c = file.shape
+    total_array = file.at[r - 1, 'Total_Goals']
+    print(total_array)
+    covered_array = file.at[r - 1, 'Covered_Goals']
+    coverage = covered_array / total_array
+    return coverage
+
+
+# find non-zero fitness for selection
+def get_fitness(pred): return pred + 1e-3 - np.min(pred)
+
+
+def translateDNA(pop):
+    state = pop.dot(2 ** np.arange(DNA_SIZE)[::-1])
+    population_size_index = state%16
+    population_size = POPULATION[population_size_index]
+
+    state = state//16
+    crossover_rate_index = state%16
+    crossover_rate = CROSSOVER_RATE[crossover_rate_index]
+
+    state = state//16
+    mutation_rate_index = state%8
+    mutation_rate = MUTATION_RATE[mutation_rate_index]
+
+    #state = state//8
+    #generation_gap_index = state%8
+    #generation_gap = GENERATION_GAP[generation_gap_index]
+
+    #state = state//8
+    #scaling_window_index = state%8
+    #scaling_window = SCALING_WINDOW[scaling_window_index]
+
+    state = state//8
+    selection_strategy_index = state
+    selection_strategy = SELECTION_STRATEGY[selection_strategy_index]
+    return population_size, crossover_rate, mutation_rate, selection_strategy
+
+
+def select(pop, fitness):    # nature selection wrt pop's fitness
+    idx = np.random.choice(np.arange(POP_SIZE), size=POP_SIZE, replace=True,
+                           p=fitness/fitness.sum())
+    return pop[idx]
+
+
+def crossover(parent, pop):     # mating process (genes crossover)
+    if np.random.rand() < CROSS_RATE:
+        i_ = np.random.randint(0, POP_SIZE, size=1)                             # select another individual from pop
+        cross_points = np.random.randint(0, 2, size=DNA_SIZE).astype(np.bool)   # choose crossover points
+        parent[cross_points] = pop[i_, cross_points]                            # mating and produce one child
+    return parent
+
+
+def mutate(child):
+    for point in range(DNA_SIZE):
+        if np.random.rand() < MUTATION_RATE_META:
+            child[point] = 1 if child[point] == 0 else 0
+    return child
+
+
+pop = np.random.randint(2, size=(POP_SIZE, DNA_SIZE))   # initialize the pop DNA
+
+
+for _ in range(N_GENERATIONS):
+    F_values = branch_coverage(translateDNA(pop))    # compute function value by extracting DNA
+
+
+    # GA part (evolution)
+    fitness = get_fitness(F_values)
+    print("Most fitted DNA: ", pop[np.argmax(fitness), :])
+    pop = select(pop, fitness)
+    pop_copy = pop.copy()
+    for parent in pop:
+        child = crossover(parent, pop_copy)
+        child = mutate(child)
+        parent[:] = child       # parent is replaced by its child
+    print('iteration number:', _)
